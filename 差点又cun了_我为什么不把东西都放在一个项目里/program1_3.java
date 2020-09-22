bf6cd9e869d422ea71aadadc666af1be42c40645
Will <zswql@hotmail.com>
2020-09-22 22:51:32 +0800

    1-3

---
+++ 差点又cun了_我为什么不把东西都放在一个项目里/program1_3.java
@@ -0,0 +1,11 @@
+package 差点又cun了_我为什么不把东西都放在一个项目里;
+
+public class program1_3 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("3.5 * 4 / 2 - 2.5 is: ");
+		System.out.println(3.5 * 4 / 2 - 2.5);
+	}
+
+}
