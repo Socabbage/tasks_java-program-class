b1bb1837025fbfe7a212157ccd79326929454bbe
Will <zswql@hotmail.com>
2020-09-22 15:35:06 +0800

    2-1

--- 差点又cun了_我为什么不把东西都放在一个项目里/Main.java
+++ 差点又cun了_我为什么不把东西都放在一个项目里/Main.java
@@ -3,8 +3,11 @@ package 差点又cun了_我为什么不把东西都放在一个项目里;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello java");
-
+		//System.out.println("Hello java");
+//		int a=5,b=4;
+//		double c;
+//		c=a/b;
+//		System.out.println("%f",c);
 	}
 
 }
---
+++ 差点又cun了_我为什么不把东西都放在一个项目里/Program2_1.java
@@ -0,0 +1,18 @@
+package 差点又cun了_我为什么不把东西都放在一个项目里;
+
+public class Program2_1 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		double radius;
+		double area;
+		
+		radius = 20;
+		
+		area = radius*radius*3.14159;
+		
+		System.out.println("The area for the circle of radius " + 
+		radius + " is " + area);
+	}
+
+}
