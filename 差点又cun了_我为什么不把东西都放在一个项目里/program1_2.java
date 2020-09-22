d3dc9e706363685ef19008739e907c0a05162cf9
Will <zswql@hotmail.com>
2020-09-22 22:51:12 +0800

    1-2

---
+++ 差点又cun了_我为什么不把东西都放在一个项目里/program1_2.java
@@ -0,0 +1,12 @@
+package 差点又cun了_我为什么不把东西都放在一个项目里;
+
+public class program1_2 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("Programming is fun");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+
+}
