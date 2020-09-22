6f0e855158025a190693f1c870eb22d4161dc779
Will <zswql@hotmail.com>
2020-09-22 22:51:47 +0800

    1-6

---
+++ 差点又cun了_我为什么不把东西都放在一个项目里/program1_6.java
@@ -0,0 +1,11 @@
+package 差点又cun了_我为什么不把东西都放在一个项目里;
+
+public class program1_6 {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("Celsius 35 is Fahrenheit degree ");
+		System.out.println((9 / 5) * 35 + 32);
+	}
+
+}
