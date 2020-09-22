64eeb94c3d006eb65b942d790c74adbde8609e6b
Will <zswql@hotmail.com>
2020-09-22 21:57:18 +0800

    2-3

--- 差点又cun了_我为什么不把东西都放在一个项目里/program2_2.java
+++ 差点又cun了_我为什么不把东西都放在一个项目里/program2_2.java
@@ -1,10 +1,17 @@
 package 差点又cun了_我为什么不把东西都放在一个项目里;
 
+import java.util.Scanner;
+
 public class program2_2 {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a number for radius:");
+		double radius = input.nextDouble();
+		double area = radius*radius*3.14159;
+		System.out.println("The area for the circle of radius " + 
+		  radius + " is " + area);
 	}
 
 }
